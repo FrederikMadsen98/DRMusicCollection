d3c4f2dc494d5dd3d59c67699700c5de963bf7ea
Klaus Eigil Erik Avgo Thor Therkildsen <klau8393@edu.zealand.dk>
2024-04-24 10:30:59 +0200

    Repository Class

---
+++ DRMusicCollection/Repository/MusicRepository.cs
@@ -0,0 +1,75 @@
+﻿using DRMusicCollection.Model;
+
+namespace DRMusicCollection.Repository
+{
+    public class MusicRepository
+    {
+        private static string newTitle = newMusicTitle;
+        private static readonly List<Music> Data = new()
+        {
+
+                new Music() {Title="House of Rock", Artist="Elvis", Duration = 3, PublicationYear=1989},
+                new Music() {Title="House of Pop", Artist="Frederik", Duration = 4, PublicationYear=1990},
+                new Music() {Title="House of Jazz", Artist="Awais", Duration = 5, PublicationYear=1991},
+                new Music() {Title="House of Metal", Artist="Ibo", Duration = 6, PublicationYear=1992}
+        };
+        public List<Music> GetAll(string title = null, string SortBy = null)
+        {
+            List<Music> musics = new (Data);
+            if(title != null)
+            {
+                musics = musics.FindAll(music => music.Title.StartsWith(title));
+
+            }
+            if (SortBy != null)
+            {
+                switch(SortBy.ToLower())
+                {
+                    case "title":
+                        musics = musics.OrderBy(music => music.Title).ToList();
+                        break;
+                    case "artist":
+                        musics = musics.OrderBy(music => music.Artist).ToList();
+                        break;
+                    case "duration":
+                        musics = musics.OrderBy(music => music.Duration).ToList();
+                        break;
+                    case "publicationYear":
+                        musics = musics.OrderBy(music => music.PublicationYear).ToList();
+                        break;
+                }
+            }
+            return musics;
+        }
+        public Music GetByTitle(string title)
+        {
+            return Data.Find(music => music.Title == title);
+        }
+        public Music Add(Music newMusic)
+        {
+            newMusic.Validate();
+            newMusic.Title = newTitle;
+            Data.Add(newMusic);
+            return newMusic;
+        }
+        public Music Delete(string title)
+        {
+            Music music = Data.Find(music1 => music1.Title == title);
+            if (music == null) return null;
+            Data.Remove(music);
+            return music;
+        }
+        public Music Update(string title, Music updates)
+        {
+            updates.Validate();
+            Music music = Data.Find(music1 =>music1.Title == title);
+            if (music == null) return null;
+            music.Title = updates.Title;
+            music.Artist = updates.Artist;
+            music.Duration = updates.Duration;
+            music.PublicationYear = updates.PublicationYear;
+            return music;
+        }
+
+    }
+}
