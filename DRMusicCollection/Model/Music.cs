7281c3717264b0195e97e657a61dbe6534ebcc61
fmads <fmads@BRUGER-PC>
2024-04-24 10:02:17 +0200

    Add project files.

---
+++ DRMusicCollection/Model/Music.cs
@@ -0,0 +1,27 @@
+﻿namespace DRMusicCollection.Model
+{
+    public class Music
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Duration { get; set; }
+        public int PublicationYear { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title}, {Artist}, {Duration}, {PublicationYear}";
+        }
+
+        public void Validate()
+        {
+            {
+                if (Title == null)
+                    throw new ArgumentNullException(nameof(Title), "Title is null");
+                if (Title.Length < 2)
+                    throw new ArgumentException("Title must be at least 2 characters:" + Title);
+                if (Duration < 0)
+                    throw new ArgumentOutOfRangeException(nameof(Duration), "Duration must be above 0 seconds:" + Duration);
+            }
+        }
+    }
+}
