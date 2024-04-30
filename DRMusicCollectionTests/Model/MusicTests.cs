2ffea7782dd2b0ff8d60a5af113a21863ee2a49f
Klaus Eigil Erik Avgo Thor Therkildsen <klau8393@edu.zealand.dk>
2024-04-30 13:03:03 +0200

    Unittest

---
+++ DRMusicCollectionTests/Model/MusicTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DRMusicCollection.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRMusicCollection.Model.Tests
+{
+    [TestClass()]
+    public class MusicTests
+    {
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
