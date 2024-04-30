4d67ad1cdda797e635575cdc4c98847281ae51dd
Klaus Eigil Erik Avgo Thor Therkildsen <klau8393@edu.zealand.dk>
2024-04-30 13:40:54 +0200

    unitTest for music class

--- DRMusicCollectionTests/Model/MusicTests.cs
+++ DRMusicCollectionTests/Model/MusicTests.cs
@@ -12,9 +12,50 @@ namespace DRMusicCollection.Model.Tests
     public class MusicTests
     {
         [TestMethod()]
-        public void ValidateTest()
+        public void ValidateTitleTest()
         {
-            Assert.Fail();
+            Music music = new()
+            {
+                Title = "House of Rock",
+                Artist = "Elvis",
+                Duration = 3,
+                PublicationYear = 1989
+            };
+            music.Validate();
+
+            Music MusicNullTitle = new()
+            {
+                Title = null,
+                Artist = "Frederik",
+                Duration = 4,
+                PublicationYear = 1990
+            };
+            Assert.ThrowsException<ArgumentException>(
+                () => MusicNullTitle.Validate());
+
+        }
+
+
+        [TestMethod()]
+        public void ValidateDuration()
+        {
+            Music MusicLowDuration = new()
+            {
+                Title = "House of Jazz",
+                Artist = "Frederik",
+                Duration = 5
+            };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => MusicLowDuration.Validate());
+
+            Music MusicGoodDuration = new()
+            {
+                Title = "Mine store tanker",
+                Artist = "Mig selv",
+                Duration = 10,
+                PublicationYear = 1992
+            };
+            MusicGoodDuration.Validate();
         }
     }
 }
